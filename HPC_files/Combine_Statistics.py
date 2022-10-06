57724421f60e88f9cb9f5d7da5c5f6d781676add
MaudBeeckmans <maud.beeckmans@outlook.com>
2022-10-06 10:08:11 +0200

    Update for GD as well

--- HPC_files/Combine_Statistics.py
+++ HPC_files/Combine_Statistics.py
@@ -5,32 +5,43 @@ Created on Wed Oct  5 17:59:30 2022
 @author: maudb
 """
 
+"""Script to combine the statistic outputs per N & T combo into a single file
+- input: $Combine_Statistics.py 'criterion' 'SD' 'ES'"""
+
+
 import os, itertools, sys
 import numpy as np
 
 def combine_output(criterion = 'IC', variable = 'Statistic', ntrials = np.array([80]), ireversal = 40, 
                    npp = np.array([60]), sd = 0.2, main_folder = os.getcwd(), 
-                   nreps = 100):
-    
+                   nreps = 100, ES = 0.8):
+    if criterion == 'IC': ES_text = ''
+    elif criterion == 'GD': ES_text = '{}ES'.format(ES)
     trials_ppcombo = np.array(list(itertools.product(ntrials, npp)))
     read_folder = os.path.join(main_folder, 'Output')
     for itrials, ipp in zip(trials_ppcombo[:, 0], trials_ppcombo[:, 1]):
     
         nreversals = int(itrials/ireversal-1)
         
-        folder = os.path.join(read_folder, 'Results{}{}SD{}T{}R{}N'.format(criterion, sd, itrials, nreversals, ipp))
+        folder = os.path.join(read_folder, 'Results{}{}SD{}{}T{}R{}N'.format(criterion, sd, 
+                                                                             ES_text, 
+                                                                             itrials, nreversals, ipp))
         Stats = np.array([])
         for irep in range(1, nreps+1):
             
             Statistic = np.load(os.path.join(folder, "{}_rep{}.npy".format(variable, irep)))
             Stats = np.append(Stats, Statistic)
-        np.save(os.path.join(main_folder, 'Stats{}{}SD{}T{}R{}N{}reps.npy'.format(criterion, sd, itrials, nreversals, ipp, nreps)), Stats)
+        np.save(os.path.join(main_folder, 'Stats{}{}SD{}{}T{}R{}N{}reps.npy'.format(criterion, sd, 
+                                                                                    ES_text, itrials, 
+                                                                                    nreversals, ipp, 
+                                                                                    nreps)), Stats)
     
 
 input_parameters = sys.argv[1:]
-assert len(input_parameters) == 2
+assert len(input_parameters) == 3
 criterion = input_parameters[0]
 SD = input_parameters[1]
+ES = input_parameters[2]
 
 
 combine_output(criterion = criterion, variable = 'Statistic', ntrials = np.arange(80, 1000, 160), 
--- HPC_files/Heatmap_simulations.py
+++ HPC_files/Heatmap_simulations.py
@@ -5,40 +5,57 @@ Created on Wed Oct  5 21:09:09 2022
 @author: maudb
 """
 
-import os, itertools
+import os, itertools, sys
 from matplotlib import pyplot as plt 
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from scipy import stats as stat
 
-tau = 0.75
+
 folder = r'C:\Users\maudb\Documents\GitHub\COMPASS\HPC_files\Output'
-
-
-
+criterion = 'GD'
+sd = 0.1
+ES = 0.8
 
 def plot3D(criterion = 'IC', ntrials = np.arange(80, 1000, 160), 
                ireversal = 40, npp = np.arange(30, 100, 20), 
-           sd = 0.2, main_folder = folder, nreps = 100, tau = 0.75):
+           sd = 0.2, main_folder = folder, nreps = 100, tau = 0.75, typeIerror = 0.05, ES = 1):
+    if criterion == 'IC': 
+        ES_text = ''
+        title = 'P(r(LRestim, LRtrue) >= {}) with nreps = {} '.format(tau, nreps)
+        tau = tau
+    elif criterion == 'GD': 
+        ES_text = '{}ES'.format(ES)
+        title = 'P(rejectH0|H0false) with alpha = {} and nreps = {}'.format(typeIerror, nreps)
+    else: 
+        print("incorrect criterion")
+        sys.exit()
     plot_folder = os.path.join(main_folder, 'Figures')
     if not os.path.isdir(plot_folder): os.makedirs(plot_folder)
+    
     fig, axes = plt.subplots(nrows = 1, ncols = 1)
     trials_ppcombo = np.array(list(itertools.product(ntrials, npp)))
     Power_df = pd.DataFrame(columns = npp, index = ntrials, dtype = 'float64')
     for itrials, ipp in zip(trials_ppcombo[:, 0], trials_ppcombo[:, 1]):
         nreversals = int(itrials/ireversal-1)
-        file = 'Stats{}{}SD{}T{}R{}N{}reps.npy'.format(criterion, sd, itrials, nreversals, ipp, nreps)
-        power = np.mean(np.load(os.path.join(main_folder, file)) >= tau)
+        file = 'Stats{}{}SD{}{}T{}R{}N{}reps.npy'.format(criterion, sd, ES_text, 
+                                                         itrials, nreversals, ipp, nreps)
+        tau = stat.t.ppf(1-typeIerror, ipp-1)
+        T_values = -np.load(os.path.join(main_folder, file))
+        # print(T_values)
+        # print(tau)
+        power = np.mean(T_values >= tau)
         Power_df.loc[itrials, ipp] = float(power)
         
         Power_array = Power_df
         
     sns.heatmap(Power_array, vmin = 0, vmax = 1, ax = axes, cmap = "viridis", annot=True, fmt='.2f')
-    title = 'P(r(LRestim, LRtrue) >= {}) with nreps = {} '.format(tau, nreps)
     fig.suptitle(title, fontweight = 'bold')
     axes.set_ylabel('trials', loc = 'top')
     axes.set_xlabel('participants', loc = 'right')
-    axes.set_title('SD = {}'.format(sd))
+    if criterion == 'IC': axes.set_title('SD = {}'.format(sd))
+    elif criterion == 'GD': axes.set_title('ES = {}'.format(ES))
     
     fig.set_size_inches((8, 6), forward=False)
     fig.tight_layout()
@@ -49,6 +66,6 @@ def plot3D(criterion = 'IC', ntrials = np.arange(80, 1000, 160),
 
     
 
-Power_df = plot3D()
+Power_df = plot3D(criterion = criterion, sd = sd, ES = ES)
         
     
