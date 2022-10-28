393e2e9b4075de3908f0884646fbdeb62f87d3c9
MaudBeeckmans <maud.beeckmans@outlook.com>
2022-10-28 10:42:33 +0200

    Update Heatmap_simulations.py
    Small changes to figure creation and saving

--- HPC_files/Heatmap_simulations.py
+++ HPC_files/Heatmap_simulations.py
@@ -14,12 +14,13 @@ from scipy import stats as stat
 
 
 folder = r'C:\Users\maudb\Documents\GitHub\COMPASS\HPC_files\Output'
-criterion = 'GD'
+criterion = 'IC'
 sd = 0.1
-ES = 0.8
+ES = 0.5
+nreps = 1000
 
 def plot3D(criterion = 'IC', ntrials = np.arange(80, 1000, 160), 
-               ireversal = 40, npp = np.arange(30, 100, 20), 
+               ireversal = 40, npp = np.arange(40, 201, 20), 
            sd = 0.2, main_folder = folder, nreps = 100, tau = 0.75, typeIerror = 0.05, ES = 1):
     if criterion == 'IC': 
         ES_text = ''
@@ -27,7 +28,7 @@ def plot3D(criterion = 'IC', ntrials = np.arange(80, 1000, 160),
         tau = tau
     elif criterion == 'GD': 
         ES_text = '{}ES'.format(ES)
-        title = 'P(rejectH0|H0false) with alpha = {} and nreps = {}'.format(typeIerror, nreps)
+        title = 'P(Ga<Gb|Ga<Gb) with alpha = {} and nreps = {}'.format(typeIerror, nreps)
     else: 
         print("incorrect criterion")
         sys.exit()
@@ -41,10 +42,12 @@ def plot3D(criterion = 'IC', ntrials = np.arange(80, 1000, 160),
         nreversals = int(itrials/ireversal-1)
         file = 'Stats{}{}SD{}{}T{}R{}N{}reps.npy'.format(criterion, sd, ES_text, 
                                                          itrials, nreversals, ipp, nreps)
-        tau = stat.t.ppf(1-typeIerror, ipp-1)
-        T_values = -np.load(os.path.join(main_folder, file))
-        # print(T_values)
-        # print(tau)
+        
+        T_values = np.load(os.path.join(main_folder, file))
+        if criterion == 'GD': 
+            tau = stat.t.ppf(1-typeIerror, ipp-1)
+            T_values = np.abs(T_values)
+
         power = np.mean(T_values >= tau)
         Power_df.loc[itrials, ipp] = float(power)
         
@@ -55,17 +58,17 @@ def plot3D(criterion = 'IC', ntrials = np.arange(80, 1000, 160),
     axes.set_ylabel('trials', loc = 'top')
     axes.set_xlabel('participants', loc = 'right')
     if criterion == 'IC': axes.set_title('SD = {}'.format(sd))
-    elif criterion == 'GD': axes.set_title('ES = {}'.format(ES))
+    elif criterion == 'GD': axes.set_title('ES = {}, Sp = {}'.format(ES, sd))
     
     fig.set_size_inches((8, 6), forward=False)
     fig.tight_layout()
-    fig.savefig(os.path.join(plot_folder, 'Heatmap{}{}SD.png'.format(criterion, sd)))
+    fig.savefig(os.path.join(plot_folder, 'Heatmap{}{}SD{}{}nreps.png'.format(criterion, sd, ES_text, nreps)))
     
     return Power_df
             
 
     
 
-Power_df = plot3D(criterion = criterion, sd = sd, ES = ES)
+Power_df = plot3D(criterion = criterion, sd = sd, ES = ES, nreps = nreps, tau = 0.75)
         
     
