22c00bdc5cdb0654932d443bd3c0a76b12c8636f
PieterVerbeke <pjverbek.verbeke@ugent.be>
2022-12-01 11:18:29 +0100

    update

--- PowerAnalysis.py
+++ PowerAnalysis.py
@@ -9,7 +9,7 @@ HPC = False
 import numpy as np
 import pandas as pd
 from multiprocessing import Pool, cpu_count
-from Functions import create_design, correlation_repetition, groupdifference_repetition, check_input_parameters
+from Functions import create_design, Incorrelation_repetition, groupdifference_repetition, check_input_parameters, Excorrelation_repetition
 from scipy import optimize
 from scipy import stats as stat
 from datetime import datetime
@@ -19,8 +19,11 @@ if HPC == False:
     import seaborn as sns
     import matplotlib.pyplot as plt
 
+import warnings
+warnings.filterwarnings('ignore')
 
-def power_estimation_correlation(npp = 30, ntrials = 480, nreversals = 12, cut_off = 0.7, high_performance = False,
+
+def power_estimation_Incorrelation(npp = 30, ntrials = 480, nreversals = 12, cut_off = 0.7, high_performance = False,
                                  nreps = 100, reward_probability = 0.8, mean_LRdistribution = 0.5, SD_LRdistribution = 0.1,
                                  mean_inverseTempdistribution = 2.0, SD_inverseTempdistribution = 1.0):
     """
@@ -31,21 +34,29 @@ def power_estimation_correlation(npp = 30, ntrials = 480, nreversals = 12, cut_o
         Number of participants in the study.
     ntrials : integer
         Number of trials that will be used to do the parameter recovery analysis for each participant.
-    nreps : integer
-        Number of repetitions that will be used for the parameter estimation process.
+    nreversals : integer
+        The number of rule-reversals that will occur in the experiment. Should be smaller than ntrials.
     cut_off : float
         Critical value that will be used to evaluate whether the repetition was successful.
     high_performance : bool (True or False)
         Defines whether multiple cores on the computer will be used in order to estimate the power.
-    nreversals : integer
-        The number of rule-reversals that will occur in the experiment. Should be smaller than ntrials.
+    nreps : integer
+        Number of repetitions that will be used for the parameter estimation process.
     reward_probability : float (element within [0, 1]), optional
         The probability that reward will be congruent with the current stimulus-response mapping rule. The default is 0.8.
+    mean_LRdistribution: float
+        Mean for the normal distribution to sample learning rates from.
+    SD_LRdistribution: float
+        Standard deviation for the normal distribution to sample learning rates from.
+    mean_inverseTempdistribution: float
+        Mean for the normal distribution to sample inverse temperatures from.
+    SD_inverseTempdistribution: float
+        Standard deviation for the normal distribution to sample inverse temperatures from.
 
     Returns
     -------
     allreps_output : TYPE
-        Pandas dataframe containing the proportion failed estimates on each repetition and the correlation value on each repetition.
+        Pandas dataframe containing the correlation value on each repetition.
     power_estimate: float [0, 1]
         The power estimation: number of reps for which the parameter recovery was successful (correlation > significance_cutoff) divided by the total number of reps.
 
@@ -53,23 +64,21 @@ def power_estimation_correlation(npp = 30, ntrials = 480, nreversals = 12, cut_o
     -----------
     Function that actually calculates the power to obtain adequate parameter estimates.
     Parameter estimates are considered to be adequate if their correlation with the true parameters is minimum the cut_off.
-    Power is calculated using a simulation-based approach.
+    Power is calculated using a Monte Carlo simulation-based approach.
     """
     start_design = create_design(ntrials = ntrials, nreversals = nreversals, reward_probability = reward_probability)
     if HPC == True: n_cpu = cpu_count()
     elif high_performance == True: n_cpu = cpu_count() - 2
     else: n_cpu = 1
 
+    #divide process over multiple cores
     pool = Pool(processes = n_cpu)
     LR_distribution = np.array([mean_LRdistribution, SD_LRdistribution])
     inverseTemp_distribution = np.array([mean_inverseTempdistribution, SD_inverseTempdistribution])
-    out = pool.starmap(correlation_repetition, [(inverseTemp_distribution, LR_distribution, npp, ntrials,
+    out = pool.starmap(Incorrelation_repetition, [(inverseTemp_distribution, LR_distribution, npp, ntrials,
                                                  start_design, rep, nreps, n_cpu) for rep in range(nreps)])
     pool.close()
     pool.join()
-
-    # allreps_output = pd.DataFrame(out, columns = ['propfailed_estimates', 'correlations'])
-    # mean_propfailed_estimates = np.mean(allreps_output['propfailed_estimates'])
 
     allreps_output = pd.DataFrame(out, columns = ['correlations'])
 
@@ -77,7 +86,85 @@ def power_estimation_correlation(npp = 30, ntrials = 480, nreversals = 12, cut_o
     power_estimate = np.mean((allreps_output['correlations'] >= cut_off)*1)
     print(str("\nPower to obtain a correlation(true_param, param_estim) >= {}".format(cut_off)
           + " with {} trials and {} participants: {}%".format(ntrials, npp, power_estimate*100)))
-    # print("\nMean failed learning rate estimates: {}%".format(np.round(mean_propfailed_estimates*100, 2)))
+
+    return allreps_output, power_estimate
+
+def power_estimation_Excorrelation(npp = 100, ntrials = 480, nreversals = 12, typeIerror = 0.05, high_performance = False,
+                                 nreps = 100, reward_probability = 0.8, mean_LRdistribution = 0.5, SD_LRdistribution = 0.1,
+                                 mean_inverseTempdistribution = 2.0, SD_inverseTempdistribution = 1.0, True_correlation = .5):
+    """
+
+    Parameters
+    ----------
+    npp : integer
+        Number of participants in the study.
+    ntrials : integer
+        Number of trials that will be used to do the parameter recovery analysis for each participant.
+    nreversals : integer
+        The number of rule-reversals that will occur in the experiment. Should be smaller than ntrials.
+    typeIerror : float
+        Critical value for p-values. From this also the cut-off for the correlation statistic can be determined.
+    high_performance : bool (True or False)
+        Defines whether multiple cores on the computer will be used in order to estimate the power.
+    nreps : integer
+        Number of repetitions that will be used for the parameter estimation process.
+    reward_probability : float (element within [0, 1]), optional
+        The probability that reward will be congruent with the current stimulus-response mapping rule. The default is 0.8.
+    mean_LRdistribution: float
+        Mean for the normal distribution to sample learning rates from.
+    SD_LRdistribution: float
+        Standard deviation for the normal distribution to sample learning rates from.
+    mean_inverseTempdistribution: float
+        Mean for the normal distribution to sample inverse temperatures from.
+    SD_inverseTempdistribution: float
+        Standard deviation for the normal distribution to sample inverse temperatures from.
+    True_correlation: float
+        The hypothesized correlation between the learning rate and the external measure theta.
+
+    Returns
+    -------
+    allreps_output : TYPE
+        Pandas dataframe containing the correlation value on each repetition, the p-value and the p-value if estimates would be perfect.
+    power_estimate: float [0, 1]
+        The power estimation: number of reps for which the parameter recovery was successful (correlation > significance_cutoff) divided by the total number of reps.
+
+    Description
+    -----------
+    Function that actually calculates the power to obtain significant correlations with external measures.
+    Parameter estimates are considered to be adequate if correctly reveal a significant correlation when a significant correlation.
+    Power is calculated using a Monte Carlo simulation-based approach.
+    """
+    start_design = create_design(ntrials = ntrials, nreversals = nreversals, reward_probability = reward_probability)
+    if HPC == True: n_cpu = cpu_count()
+    elif high_performance == True: n_cpu = cpu_count() - 2
+    else: n_cpu = 1
+
+    #Use beta_distribution to determine the p-value for the hypothesized correlation
+    beta_distribution = stat.beta((npp/2)-1, (npp/2)-1, loc = -1, scale = 2)
+    true_pValue = 1-beta_distribution.cdf(True_correlation)
+
+    print(str("\np-value for true correlation is :{}%\n".format(true_pValue)))
+
+    #divide process over multiple cores
+    pool = Pool(processes = n_cpu)
+    LR_distribution = np.array([mean_LRdistribution, SD_LRdistribution])
+    inverseTemp_distribution = np.array([mean_inverseTempdistribution, SD_inverseTempdistribution])
+    out = pool.starmap(extcorrelation_repetition, [(inverseTemp_distribution, LR_distribution, True_correlation, npp, ntrials,
+                                                 start_design, rep, nreps, n_cpu) for rep in range(nreps)])
+    pool.close()
+    pool.join()
+
+    allreps_output = pd.DataFrame(out, columns = ['Statistic','estimated_pValue', 'True_pValue'])
+
+    #Compute power if estimates would be perfect.
+    power_true = np.mean((allreps_output['True_pValue'] <= typeIerror)*1)
+    print(str("\nPower to obtain a significant correlation if parameter estimates were perfect {}%".format(power_true*100)))
+
+    #Compute power for correlation with estimated parameter values.
+    power_estimate = np.mean((allreps_output['estimated_pValue'] <= typeIerror)*1)
+    print(str("\nPower to obtain a significant correlation between model parameter and an external measure that is {} correlated".format(True_correlation)
+          + " with {} trials and {} participants: {}%".format(ntrials, npp, power_estimate*100)))
+
     return allreps_output, power_estimate
 
 def power_estimation_groupdifference(npp_per_group = 20, ntrials = 480, nreps = 100, typeIerror = 0.05,
@@ -95,53 +182,66 @@ def power_estimation_groupdifference(npp_per_group = 20, ntrials = 480, nreps = 
         Number of trials that will be used to do the parameter recovery analysis for each participant.
     nreps : integer
         Number of repetitions that will be used for the parameter estimation process.
-    cut_off : float
-        Critical value that will be used to evaluate whether the repetition was successful.
+    typeIerror : float
+        Critical value for p-values. From this also the cut-off for the correlation statistic can be determined.
     high_performance : bool (True or False)
         Defines whether multiple cores on the computer will be used in order to estimate the power.
     nreversals : integer
         The number of rule-reversals that will occur in the experiment. Should be smaller than ntrials.
     reward_probability : float (element within [0, 1]), optional
         The probability that reward will be congruent with the current stimulus-response mapping rule. The default is 0.8.
+    mean_LRdistributionG1: float
+        Mean for the normal distribution to sample learning rates for group 1.
+    SD_LRdistributioG1: float
+        Standard deviation for the normal distribution to sample learning rates for group 1.
+    mean_inverseTempdistributionG1: float
+        Mean for the normal distribution to sample inverse temperatures for group 1.
+    SD_inverseTempdistributionG1: float
+        Standard deviation for the normal distribution to sample inverse temperatures for group 1.
+    mean_LRdistributionG2: float
+        Mean for the normal distribution to sample learning rates for group 2.
+    SD_LRdistributioG2: float
+        Standard deviation for the normal distribution to sample learning rates for group 2.
+    mean_inverseTempdistributionG2: float
+        Mean for the normal distribution to sample inverse temperatures for group 2.
+    SD_inverseTempdistributionG2: float
+        Standard deviation for the normal distribution to sample inverse temperatures for group 2.
 
     Returns
     -------
     allreps_output : TYPE
-        Pandas dataframe containing the proportion failed estimates on each repetition and the p-value on each repetition.
+        Pandas dataframe containing the p-value on each repetition.
     power_estimate: float [0, 1]
-        The power estimation: number of reps for which the parameter recovery was successful (significant group difference found) divided by the total number of reps.
+        The power estimation: number of reps for which a significant group difference was found divided by the total number of reps.
 
     Description
     -----------
     Function that actually calculates the power to obtain adequate parameter estimates.
     Parameter estimates are considered to be adequate if they correctly reveal the group difference when a true group difference of size 'cohens_d' exists.
-    Power is calculated using a simulation-based approach.
+    Power is calculated using a Monte Carlo simulation-based approach.
     """
 
     start_design = create_design(ntrials = ntrials, nreversals = nreversals, reward_probability = reward_probability)
     if high_performance == True: n_cpu = cpu_count() - 2
     else: n_cpu = 1
     if __name__ == '__main__':
-        # First: check what the power is when true parameters would be recoverable
+        # First: check what the power is when parameter estimations are perfect
         if HPC == False:
             power = tt_ind_solve_power(nobs1 = npp_per_group, ratio = 1, effect_size = cohens_d, alpha = typeIerror, power = None,
                                     alternative = 'larger')
             print("\nPower if estimates would be perfect: {}%".format(np.round(power, 4)*100))
 
-
-
+        #divide process over multiple cores
         LR_distributions = np.array([[mean_LRdistributionG1, SD_LRdistributionG1], [mean_LRdistributionG2, SD_LRdistributionG2]])
         inverseTemp_distributions = np.array([[mean_inverseTempdistributionG1, SD_inverseTempdistributionG1],
                                               [mean_inverseTempdistributionG2, SD_inverseTempdistributionG2]])
-
-
         pool = Pool(processes = n_cpu)
         out = pool.starmap(groupdifference_repetition, [(inverseTemp_distributions, LR_distributions, npp_per_group,
                                                      ntrials, start_design, rep, nreps, n_cpu, False) for rep in range(nreps)])
         # before calling pool.join(), should call pool.close() to indicate that there will be no new processing
         pool.close()
         pool.join()
-        # allreps_output = pd.DataFrame(out, columns = ['propfailed_estimates', 'p_values'])
+
         allreps_output = pd.DataFrame(out, columns = ['Statistic', 'PValue'])
 
         # check for which % of repetitions the group difference was significant
@@ -150,12 +250,9 @@ def power_estimation_groupdifference(npp_per_group = 20, ntrials = 480, nreps = 
         print(str("\nPower to detect a significant group difference when the estimated effect size d = {}".format(cohens_d)
               + " with {} trials and {} participants per group: {}%".format(ntrials,
                                                                          npp_per_group, power_estimate*100)))
-        # print("\nMean failed learning rate estimates per repetition: {}%".format(np.round(mean_propfailed_estimates*100, 2)))
         return allreps_output, power_estimate
 
 #%%
-
-
 import os, sys
 
 if __name__ == '__main__':
@@ -195,7 +292,7 @@ if __name__ == '__main__':
             tau = InputDictionary['tau'][row]
             s_pooled = sdLR
 
-            output, power_estimate = power_estimation_correlation(npp = npp, ntrials = ntrials, nreps = nreps,
+            output, power_estimate = power_estimation_Incorrelation(npp = npp, ntrials = ntrials, nreps = nreps,
                                                                   cut_off = tau,
                                                high_performance = full_speed, nreversals = nreversals,
                                                reward_probability = reward_probability, mean_LRdistribution = meanLR,
@@ -242,7 +339,32 @@ if __name__ == '__main__':
                 axes.set_title("Power = {}% based on {} reps with ES = {}".format(np.round(power_estimate*100, 2), nreps, cohens_d))
                 axes.axvline(x = tau, lw = 2, linestyle ="dashed", color ='k', label ='tau')
 
+        elif criterion == "EC":
+            npp = InputDictionary['npp'][row]
+            meanLR, sdLR = InputDictionary['meanLR'][row], InputDictionary['sdLR'][row]
+            meanInverseT, sdInverseT = InputDictionary['meanInverseTemperature'][row], InputDictionary['sdInverseTemperature'][row]
+            True_correlation = InputDictionary['True_correlation'][row]
+            typeIerror = InputDictionary['TypeIerror'][row]
+            s_pooled = sdLR
 
+            beta_distribution = stat.beta((npp/2)-1, (npp/2)-1, loc = -1, scale = 2)
+            tau = -beta_distribution.ppf(typeIerror)
+
+            output, power_estimate = power_estimation_Excorrelation(npp = npp, ntrials = ntrials, nreps = nreps,
+                                                                  typeIerror = typeIerror,
+                                               high_performance = full_speed, nreversals = nreversals,
+                                               reward_probability = reward_probability, mean_LRdistribution = meanLR,
+                                               SD_LRdistribution = sdLR, mean_inverseTempdistribution = meanInverseT,
+                                               SD_inverseTempdistribution = sdInverseT)
+            output.to_csv(os.path.join(output_folder, 'OutputEC{}SD{}TC{}T{}R{}N{}M.csv'.format(s_pooled, True_correlation, ntrials,
+                                                                                      nreversals,
+                                                                                      npp, nreps)))
+            if HPC == False:
+                fig, axes = plt.subplots(nrows = 1, ncols = 1)
+                sns.kdeplot(output["Statistic"], label = "T", ax = axes)
+                fig.suptitle("P(p-value <= {}) with {} pp, {} trials".format(typeIerror, npp, ntrials), fontweight = 'bold')
+                axes.set_title("Power = {}% based on {} reps with true correlation {}".format(np.round(power_estimate*100, 2), nreps, True_correlation))
+                axes.axvline(x = tau, lw = 2, linestyle ="dashed", color ='k', label ='tau')
 
 
 
